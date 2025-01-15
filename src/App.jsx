85afe2da58ec8eedc2cf7508c8b85cc7a5f18694
gouttedooo <goudal.donovan@live.fr>
2025-01-15 12:02:35 +0100

    "first commit, initialisation du projet"

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles/main.scss";
+function App() {
+  return (
+    <>
+      <Background />
+      <header>
+        <Entete />
+        <Logo />
+      </header>
+      <main>
+        <Days />
+        <Tournaments />
+      </main>
+      <footer>
+        <Contacts />
+      </footer>
+    </>
+  );
+}
+
+export default App;
